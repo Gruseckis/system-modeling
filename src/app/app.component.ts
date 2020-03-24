769feac7c173333217ec055e11686a8d204e495f
Germans Gruseckis <g.gruseckis@gmail.com>
2020-03-24 23:50:42 +0200

    Fix random algorithm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,23 @@
-import { Component } from "@angular/core";
-import { ModelSystem } from "./shared/system";
+import { Component } from '@angular/core';
+import { ModelSystem } from './shared/system';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public showTable = false;
+  public showTable = true;
   public timerSpeed = new FormControl(200);
   public endTime = new FormControl(500);
   public modeler = new ModelSystem(200); // TODO: Change to 500
 
-  public showResults() {
-    // console.log(this.modeler.machineStatus);
-    this.showTable = !this.showTable;
+  public downloadResults() {
+    this.modeler.downloadCsv();
   }
 
   public onStart() {
-    this.modeler.visualizeData(this.endTime,this.timerSpeed);
+    this.modeler.visualizeData(this.endTime, this.timerSpeed);
   }
 }
